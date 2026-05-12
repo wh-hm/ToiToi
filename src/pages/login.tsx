38061d2e52b1cc155c85eb24da3a7e5a130d49a7
Reo0727-png <146697597+Reo0727-png@users.noreply.github.com>
2026-05-12 15:00:26 +0900

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import LoginForm from '../features/auth/LoginForm';
 
+const LoginPage: React.FC = () => {
+  return (
+    <div className="login-container">
+      <h1>ToiToi</h1>
+        
+        <div>
+          <button>サインアップ</button>
+        </div>
+
+        <div>
+          <button>サインイン</button>
+        </div>
+    
+    </div>
+  );
+}
+    
