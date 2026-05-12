0672ca80fee23c9257a1550617e8724e19880657
Reo0727-png <146697597+Reo0727-png@users.noreply.github.com>
2026-05-12 14:03:21 +0900

    Update login.tsx

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,1 +1,19 @@
+export default function LoginPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>ToiToi</h1>
+
+      <form>
+        <div>
+          <button>サインアップ</button>
+        </div>
+
+        <div>
+          <button>サインイン</button>
+        </div>
+
+      </form>
+    </div>
+  );
+}
 
